2831cdaa4669e38455163c03ea8f7eddbe4ea1b7
Corina <calin@DESKTOP-05R1BR5>
2023-04-04 20:53:23 +0300

    /addproducts path

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Home } from './components/Home';
+import { AddProducts } from './components/AddProducts'
 
 export class App extends Component{
   render(){
     return(
       <BrowserRouter>
           <Switch>
-            <Route exact path='/' component={Home} />  
+            <Route exact path='/' component={Home} />
+            <Route path='/addproducts' component={AddProducts} />  
           </Switch> 
       </BrowserRouter>
     )
