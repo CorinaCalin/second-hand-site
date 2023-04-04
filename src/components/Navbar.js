5aed712d67015f07a9d6392a0021d9b9cb4385e3
Corina <calin@DESKTOP-05R1BR5>
2023-04-04 20:16:24 +0300

    adding logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import logo from '../images/image.svg'
 
 export const Navbar = () => {
 return(
-    <div>
-Products
+    <div className='navbox'>
+        <div className='leftside'>
+            <img src={logo} alt=""/>
+        </div>
+        <div className='rightside'></div>
     </div>
 )
 }
