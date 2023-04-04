43637a2aa5043d110f01378eae57d1ee06230156
Corina <calin@DESKTOP-05R1BR5>
2023-04-04 21:08:27 +0300

    add products page

--- src/components/AddProducts.js
+++ src/components/AddProducts.js
@@ -2,8 +2,20 @@ import React from "react";
 
 export const AddProducts = () => {
     return(
-        <div>
-            add products
+        <div className='container'>
+            <br />
+                <h2>ADD PRODUCTS</h2>
+                    <hr/>
+            <form autoComplete="off" className="form-group">
+                <label htmlFor="product-name">Product name</label> <br />
+                <input type="text" className="form-control" required />
+
+                <label htmlFor="product-price">Product price</label> <br />
+                <input type="number" className="form-control" required />
+
+                <input type="file" /> <br />
+                <button className="btn btn-success btn-md">ADD</button>
+            </form>
         </div>
     )
 }
