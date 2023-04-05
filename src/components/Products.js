03b1340fd246b4b1178aee715e5dee4914bb03ce
Corina <calin@DESKTOP-05R1BR5>
2023-04-05 12:34:11 +0300

    products page

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React,{useContext}  from 'react'
+import { ProductsContext } from '../global/ProductsContext'
+
 
 export const Products = () => {
+
+    const {products} = useContext(ProductsContext);
+    console.log(products);
+
 return(
-    <div>
-Products
-    </div>
+    <>
+            {products.length !== 0 && <h1>Products</h1>}
+            <div className='products-container'>
+                {products.length === 0 && <div>No products to display</div>}
+                {products.map(product => (
+                    <div className='product-card' key={product.ProductID}>
+                        <div className='product-img'>
+                            <img src={product.ProductImg} alt="not found" />
+                        </div>
+                        <div className='product-name'>
+                            {product.ProductName}
+                        </div>
+                        <div className='product-price'>
+                            Rs {product.ProductPrice}.00
+                    </div>
+                        <button className='addcart-btn'>ADD TO CART</button>
+                    </div>
+                ))}
+            </div>
+        </>
 )
 }
